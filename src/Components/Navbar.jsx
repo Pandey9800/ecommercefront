d877975c993d8382c192cced4c8152def43eb1ec
Ravi Pandey <67542571+Pandey9800@users.noreply.github.com>
2024-07-11 14:08:47 +0530

    added images

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Imgrefresher from "./images/logo1.png";
 
 const Navbar = () => {
   return (
       <div>
+          <div className="navbar_div">
+              <div>
+                  <input type="text" placeholder="Search here" />
+                  {/* searchIcon */}
+                  {/* closeIcon */}
+
+              </div>
+              <div>
+                  <div className="navbar_main">
+                      <div>
+                          <div>
+                              {/* search Icon */}
+                          </div>
+                          {/* Menu Icon */}
+                      </div>
+                      <div>
+                          <img src="" alt="" />
+                      </div>
+                  </div>
+              </div>
+          </div>
           <Link to="/">Home</Link>
           <Link to="/contact">Contact</Link>
           <Link to="/privacy">Privacy</Link>
